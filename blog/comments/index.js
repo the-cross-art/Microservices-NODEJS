0881861aa438e47c9544f5976fcf133c04757723
the-cross-art <imrannaz326@gmail.com>
2021-03-22 01:32:10 +0530

    query and Event Bus Added

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
 
+const axios = require('axios');
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -13,7 +15,7 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
 
@@ -22,7 +24,23 @@ app.post('/posts/:id/comments', (req, res) => {
     comments.push({id:commentId, content});
 
     commentsByPostId[req.params.id] = comments;
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    });
+
     res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+    console.log('Received Event:', req.body.type);
+
+    res.send({});
 });
 
 app.listen(4001, () => {
