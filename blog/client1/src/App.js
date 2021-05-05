b475a56141978cb0dac6a053323972b910f2002b
the-cross-art <imrannaz326@gmail.com>
2021-05-05 23:56:05 +0530

    typeScript started

--- blog/client1/src/App.js
+++ blog/client1/src/App.js
@@ -4,7 +4,7 @@ import PostList from './PostList';
 export default () => {
     return (
         <div className="container">
-            <h1>Create a Post</h1>
+            <h1>Create a Post!!!!!!</h1>
             <PostCreate />
             <hr />
             <h1>Posts</h1>
